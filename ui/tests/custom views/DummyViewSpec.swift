1989c1564bd13a404ead0fe3cf29a94c76784a35
傅立业 <17433201@qq.com>
2018-06-22 22:48:53 +0800

    Fixed some wording.

--- ui/tests/custom views/DummyViewSpec.swift
+++ ui/tests/custom views/DummyViewSpec.swift
@@ -25,7 +25,7 @@ internal class DummyViewSpec : QuickSpec {
 				expect(dummyView.hitTest(CGPoint(x: 50, y: 50), with: nil)).to(beNil())
 			}
 			
-			it("should alllow its subviews respond to touches") {
+			it("should alllow its subviews to respond to touches") {
 				let childView = UIView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
 				dummyView.addSubview(childView)
 				
