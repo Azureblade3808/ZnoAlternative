0770787de723ba37c74fb25a7a4c6783adb4b5d5
傅立业 <17433201@qq.com>
2018-06-22 22:45:31 +0800

    Changed some wording and simplified process for 'ui/DummyView' tests.

--- ui/tests/custom views/DummyViewSpec.swift
+++ ui/tests/custom views/DummyViewSpec.swift
@@ -14,38 +14,22 @@ import ui
 internal class DummyViewSpec : QuickSpec {
 	override
 	internal func spec() {
-		describe("DummyView") {
+		describe("`DummyView`") {
 			var dummyView: DummyView!
 			
 			beforeEach {
-				dummyView = DummyView(frame: CGRect(x: 0, y: 0, width: 101, height: 101))
+				dummyView = DummyView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
 			}
 			
-			it("should not respond to touches") {
-				expect(dummyView.hitTest(CGPoint(x: 0, y: 0), with: nil)).to(beNil())
-				expect(dummyView.hitTest(CGPoint(x: 50, y: 0), with: nil)).to(beNil())
-				expect(dummyView.hitTest(CGPoint(x: 100, y: 0), with: nil)).to(beNil())
-				expect(dummyView.hitTest(CGPoint(x: 0, y: 50), with: nil)).to(beNil())
+			it("should not respond to touches itself") {
 				expect(dummyView.hitTest(CGPoint(x: 50, y: 50), with: nil)).to(beNil())
-				expect(dummyView.hitTest(CGPoint(x: 100, y: 50), with: nil)).to(beNil())
-				expect(dummyView.hitTest(CGPoint(x: 0, y: 100), with: nil)).to(beNil())
-				expect(dummyView.hitTest(CGPoint(x: 50, y: 100), with: nil)).to(beNil())
-				expect(dummyView.hitTest(CGPoint(x: 100, y: 100), with: nil)).to(beNil())
 			}
 			
-			it("should have its children respond to touches") {
-				let childView = UIView(frame: CGRect(x: 0, y: 0, width: 101, height: 101))
+			it("should alllow its subviews respond to touches") {
+				let childView = UIView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
 				dummyView.addSubview(childView)
 				
-				expect(dummyView.hitTest(CGPoint(x: 0, y: 0), with: nil)) == childView
-				expect(dummyView.hitTest(CGPoint(x: 50, y: 0), with: nil)) == childView
-				expect(dummyView.hitTest(CGPoint(x: 100, y: 0), with: nil)) == childView
-				expect(dummyView.hitTest(CGPoint(x: 0, y: 50), with: nil)) == childView
 				expect(dummyView.hitTest(CGPoint(x: 50, y: 50), with: nil)) == childView
-				expect(dummyView.hitTest(CGPoint(x: 100, y: 50), with: nil)) == childView
-				expect(dummyView.hitTest(CGPoint(x: 0, y: 100), with: nil)) == childView
-				expect(dummyView.hitTest(CGPoint(x: 50, y: 100), with: nil)) == childView
-				expect(dummyView.hitTest(CGPoint(x: 100, y: 100), with: nil)) == childView
 			}
 		}
 	}
